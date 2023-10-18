6206b071057cc68c2caefe3399335a0148fb3e17
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-10-19 00:23:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function threeSum(arr, target) {
 // write your code here
 	let ans = Number.MAX_VALUE;
 	let n = arr.length;
-	for(let i=0;i<n;i++){
+	for(let i=0;i<n-2;i++){
 		for(let j=i+1;j<n-1;j++){
-			let k=j+1;k<n-2;k++){
+			let k=j+1;k<n;k++){
 				let sum = arr[i]+arr[j]+arr[k]; 
 				let Dif = (sum - target);
 				if(ans > Dif){
