07206579f1f3984612304d524fd4f7e7221c0e6c
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-10-19 00:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let ans = Number.MAX_VALUE;
+	let n = arr.length;
+	for(let i=0;i<n;i++){
+		for(let j=i+1;j<n-1;j++){
+			let k=j+1;k<n-2;k++){
+				let sum = arr[i]+arr[j]+arr[k]; 
+				let Dif = (sum - target);
+				if(ans > Dif){
+					ans = Dif;
+				}
+			}
+		}
+	}
+  return ans;
 }
 
 module.exports = threeSum;
